87b9d87287bf60c53fb851eb3a1fb5a1174fc095
Arkadiusz Smagała <asmagala@outlook.com>
2020-08-01 13:05:26 +0200

    Init.

---
+++ models/department.model.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const departmentSchema = new mongoose.Schema({
+  name: { type: String, required: true, minlength: 5, maxlength: 20 }
+});
+
+module.exports = mongoose.model('Department', departmentSchema);
+
